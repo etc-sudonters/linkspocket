dd231be1d46086ed8ca6a789af15b4ed8a12b458
anr <alecreiter@gmail.com>
2023-09-23 16:44:54 -0400

    Scripts to create ootr seeds

--- pocket.sh
+++ pocket.sh
@@ -1,14 +1,35 @@
 #!/usr/bin/env bash
 set -e
-
+SCRIPT_DIR=$( cd -- "$( dirname -- $(realpath "${BASH_SOURCE[0]}") )" &> /dev/null && pwd )
 ROM=""
 OUT=""
 VERSION="v7.1.0"
 TAG=""
 
 usage() {
-    echo "Usage $0 -r <path to oot rom> -d <directory to put output into> [-v <zootr version>] [-t <output tag>] [-- [additional zootr args]]" 1>&2;
+    cat <<EOF
+Usage $0 -r <path to oot rom> -o <directory to put output into> 
 
+    -h Show this message and exit
+    -o Base output directory, may be absolute or relative to current directory
+       This directory will be created if necessary
+    -r Path to rom, may be absolute or relative to current directory
+    -t Output files are placed in a directory with this name
+       By default a tag is derived from the generated settings file
+    -v Version of the randomizer to generate with, default is 'v7.1.0'
+       If the version is not present, this script will attempt to build it first
+       May be any 'commit-ish' (as defined by git) present in the OOTR repository
+
+OOTR specific CLI options can be passed by providing -- and then the options.
+A settings file is always generated.
+
+Example usage:
+
+    $0 -r ./my-rom.z64 -o ~/zootr-seeds/ -t my-seed -- --settings_string [omitted]
+
+This would create a directory ~/zootr-seeds/my-seed from the specified rom and
+settings string.
+EOF
 }
 
 while getopts "hr:o:v:t:" o; do 
@@ -38,22 +59,33 @@ done
 
 shift $((OPTIND-1))
 
-if [ -z "${ROM}" ] || [ ! -f "${ROM}" ]; then
+if [ -z "${ROM}" ];  then
     echo "-r <path to rom> must be provided!"
     usage
     exit 2
 fi
 
+if [ ! -f "${ROM}" ]; then
+    echo "${ROM} does not exist!"
+    usage
+    exit 3
+fi
+
 if [ -z "${OUT}" ]; then
     echo "-o <output directory> must be provided!"
     usage
-    exit 3
+    exit 4
 fi
 
 REALROM=$(realpath -e "${ROM}")
 ROMNAME=$(basename "${ROM}")
 mkdir -p /var/tmp/linkspocket
 TMPOUT=$(mktemp -d -p /var/tmp/linkspocket/)
+
+if [ "$(docker images -q "zootr:${VERSION}" 2> /dev/null)" == "" ]; then
+    echo "Version ${VERSION} not found locally, building..."
+    ZOOTR_REF="${VERSION}" "${SCRIPT_DIR}/zootr/build.sh"
+fi
 
 CONTID=$(docker create \
     -v "${REALROM}:/etc/zootr/${ROMNAME}:ro" \
@@ -71,7 +103,8 @@ docker cp "${CONTID}:/lib/zootr/Output" "${TMPOUT}"
 if [ -z "${TAG}" ]; then
     SETTINGSFILE=$(ls -1 "${TMPOUT}/Output" | grep -i 'settings')
 
-    TAG=$(jq -jr '.["file_hash"] | join(" ")' "${TMPOUT}/Output/${SETTINGSFILE}" | tr -sc '[:alnum:]' '-' | tr '[:upper:]' '[:lower:]')
+    TAG=$(jq -jr '.["file_hash"] | join(" ")' "${TMPOUT}/Output/${SETTINGSFILE}" |\
+          tr -sc '[:alnum:]' '-' | tr '[:upper:]' '[:lower:]')
     if [ -z "${TAG}" ]; then 
         echo "Unable to determine tag automatically" 1>&2
         exit 4
--- zootr/build.sh
+++ zootr/build.sh
@@ -1,13 +1,21 @@
 #!/usr/bin/env bash
 set -ex
-
+SCRIPT_DIR=$( cd -- "$( dirname -- $(realpath "${BASH_SOURCE[0]}") )" &> /dev/null && pwd )
 REPO="git@github.com:OoTRandomizer/OoT-Randomizer.git"
-REF="v7.1.0"
+REF="${ZOOTR_REF:-'v7.1.0'}"
 
 if [ -d "zootr-source" ]; then
     rm -fr "zootr-source"
 fi
 
-git clone --quiet --progress --branch "${REF}" --single-branch --depth 1 "${REPO}" zootr-source
+TMPOUT=$(mktemp -d -p /var/tmp/linkspocket/)
+pushd "${TMPOUT}"
+if [ "$(pwd)" != "${TMPOUT}" ]; then
+    echo "whoopps..."
+    exit 99
+fi
+trap "rm ${TMPOUT}; popd" 9
+
+git clone --quiet --progress --branch "${REF}" --single-branch --depth 1 "${REPO}" "zootr-source"
 rm -rf ./zootr-source/.git
-docker build -t "zootr:${REF}" .
+docker build -f "${SCRIPT_DIR}/Dockerfile" -t "zootr:${REF}" --build-context "base=${SCRIPT_DIR}" .
